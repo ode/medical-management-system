218ceaa6f495cb6de11b8926bf784522fa6b947e
gautamjajoo <gautamjajoo1729@gmail.com>
2022-12-03 16:21:46 +0530

    update appointment class

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,28 +1,36 @@
 import java.io.*;
 import java.util.Scanner;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.sql.Timestamp;
 
 public class Appointment {
+    private String getAppointment;
     private int appointmentId;
-    private User user;
-    private datetime dateTime;
+//    private User user;
+//    private datetime dateTime;
     private Doctor doctor;
 
     Appointment() {
 
     }
 
-    public String getAppointmentDetails(String bitsId) {
+    public String getAppointmentDetails(String bitsId) throws Exception{
         try
         {
-            FileInputStream fis = new FileInputStream("userList.txt");
+            String app = null;
+            FileInputStream fis = new FileInputStream("appointmentList.txt");
             Scanner sc = new Scanner(fis);
             while(sc.hasNextLine())
             {
-                String[] s = sc.nextLine().split(";");
+
+                String[] s = sc.nextLine().split(",");
                 if(s[1] == bitsId) {
-                    return s.toString();
+                    app = s.toString();
+                    break;
                 }
             }
+            return app;
             sc.close();
         }
         catch(IOException e)
@@ -31,9 +39,41 @@ public class Appointment {
         }
     }
 
-    public void bookAppointment(String s) {
-        if(checkAppointment(s)) {
-            // append
+    public void bookAppointment(String bitsId) throws Exception{
+        System.out.println("With whom you want to book an appointment with? Please enter the Doctor_id.");
+
+        // Timestamp: https://www.javatpoint.com/java-timestamp
+        Timestamp instant = Timestamp.from(Instant.now());
+        String appointment = instant.toString();
+        appointment = appointment + "," + bitsId;
+
+        Scanner sc = new Scanner(System.in);
+        appointment = appointment + "," + sc.nextLine();
+
+        // Display the doctor's available day and time - to be implemented
+        System.out.println("Please enter the day and the time of the appointment in the given format");
+        appointment = appointment + "," + sc.nextLine();
+        appointment = appointment + "," + sc.nextLine();
+
+        if(isDoctorAvailable(appointment)) {
+            if(checkAppointment(appointment)) {
+                System.out.println("Appointment booked");
+                try {
+                    BufferedWriter out = new BufferedWriter(new FileWriter("appointmentList.txt"));
+                    out.write(appointment + "\n");
+                    out.close();
+                }
+                catch (IOException e) {
+                    System.out.println("Appointment could not be booked" + e);
+                }
+            }
+            else {
+                System.out.println("Appointment could not be booked since there is " +
+                        "an appointment at the given time slot");
+            }
+        }
+        else {
+            System.out.println("Sorry, Doctor is not available at the given time");
         }
     }
 
@@ -42,14 +82,43 @@ public class Appointment {
         {
             FileInputStream fis = new FileInputStream("appointmentList.txt");
             Scanner sc = new Scanner(fis);
-            String[] updatedAppointment = appointment.split(";");
-            String[] time = updatedAppointment[1].split(":")
+            String[] updatedAppointment = appointment.split(",");
+
+            String day = updatedAppointment[3];
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+            LocalTime dateTime = LocalTime.parse(updatedAppointment[4], formatter);
+
+            int count = 0;
+            int numberOfLines = 0;
+
             while(sc.hasNextLine())
             {
-//                String[] s = sc.nextLine().split(";");
-//                if(s[1] == bitsId) {
-//                    return s.toString();
-//                }
+                String[] s = sc.nextLine().split(",");
+                String dayForCheck = s[3];
+
+                numberOfLines++;
+
+                DateTimeFormatter formatterForFile = DateTimeFormatter.ofPattern("HH:mm");
+                LocalTime dateTimeCheck = LocalTime.parse(updatedAppointment[4], formatterForFile);
+
+                if(updatedAppointment[1].equals(s[1])) {
+                    if(dayForCheck.equals(day)) {
+                        if(dateTime.isAfter(dateTimeCheck.minusMinutes(10))
+                                && dateTime.isBefore(dateTimeCheck.plusMinutes(10))) {
+                            count++;
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+            if(count == numberOfLines) {
+                return true;
             }
             sc.close();
         }
@@ -57,5 +126,14 @@ public class Appointment {
         {
             e.printStackTrace();
         }
+    }
+
+    private boolean isDoctorAvailable(String appointment) {
+        String[] updatedAppointment = appointment.split(",");
+        String doctorId = updatedAppointment[2];
+        // call Notice Board for doctor data
+//        if() {
+//
+//        }
     }
 }
